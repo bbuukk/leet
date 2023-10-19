31961013a598c7ee4c1c04bb92333b500c5efb69
bb <bohdan.buchok@ukma.edu.ua>
2023-10-19 18:02:54 +0300

    to be or not to be

---
+++ 30_days_js/to_be_or_not_to_be.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  return {
+    toBe(expected) {
+      if (expected === val) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe(unexpected) {
+      if (unexpected !== val) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+  };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
